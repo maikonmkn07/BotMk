ea7a4449dbff79f44993af91dc96adf1c3bf7734
maikonmkn07 <66918606+maikonmkn07@users.noreply.github.com>
2021-01-10 23:39:22 -0300

    Add files via upload

---
+++ src/darkmenu.js
@@ -0,0 +1,39 @@
+const darkmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Maikon:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *MAIKON*
+  WA.me/5544999013362
+╚════════════════════`
+}
+
+exports.darkmenu = darkmenu
+
+
+
+
+
+
+
+
---
+++ src/menu1.js
@@ -0,0 +1,76 @@
+const menu1 = (prefix) => {
+	return `
+
+
+━━━━━━━━━━━━━━━━━━━━━                   
+     ╔══════════════╗
+          ✯ MK MODS ✯
+     ╚══════════════╝
+━━━━━━━━━━━━━━━━━━━━━
+
+         *Aqui é o @bot* 🤳⚡
+               
+ 👉Coloque Comando escolhido👈
+
+ *Comando: 👉.m01* 
+ *Descrição:* Link dos Apks Mods
+
+ *Comando: 👉.m02* 
+ *Descrição:* Não baixa audio,vídeo,status 
+
+ *Comando: 👉.m03* 
+ *Descrição:* Sobre GBWhatsApp - Sam Mods
+
+ *Comando: 👉.m04* 
+ *Descrição:* Sobre Yxwhatsapp - Clebersonjr
+
+ *Comando: 👉.m05*
+ *Descrição:* Sobre GBWhatsApp - Alex Mods
+
+ *Comando: 👉.m06* 
+ *Descrição:* Sobre Whatsapp Aero - Hazar
+
+ *Comando: 👉.m07*
+ *Descrição:* Migrar pro GBWhatsApp sem perder as conversas
+
+ *Comando: 👉.m08* 
+ *Descrição:* Funções do Spotify da descrição
+
+ *Comando: 👉.m09* 
+ *Descrição:* Sobre menssagens temporárias
+
+ *Comando: 👉.m10* 
+ *Descrição:* Sobre Tiktok Pro
+
+ *Comando: 👉.m11*
+ *Descrição:* Sobre Whatsapp Business Mod - Sam Mods
+
+ *Comando: 👉.m12*
+ *Descrição:* Sobre YouTube Vanced
+
+ *Comando: 👉.m13* 
+ *Descrição:* Sobre Instagram Pro
+
+ *Comando: 👉.m14* 
+ *Descrição:* Sobre Pulsar Music Pro
+
+ *Comando: 👉.m15* 
+ *Descrição:* Sobre Tag Scanner Pro
+
+ *Comando: 👉.m16* 
+ *Descrição:* Sobre Whatsapp Business Mods
+
+ *Comando: 👉.m17* 
+ *Descrição:* Grupo de Comédia vídeos engraçados e de descontração.
+
+ *Comando: 👉.m18* 
+ *Descrição:* Grupo de Figurinhas
+
+ *Comando: 👉.menu2*
+ *Descrição:* Menu 2 com mais funções do Bot.
+ 
+ *Digite a baixo o número desejado exemplo: *.m01* ou *.m02* ....👇
+ ━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.menu1 = menu1
